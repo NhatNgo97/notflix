b8299f146380b1c580d68345caa4d89140d04867
NhatNgo97 <nhatngo9799@gmail.com>
2022-04-11 02:33:52 +1000

    banner UI

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./banner.css";
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 function Banner() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const API_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
 
   useEffect(() => {
     axios
       .get(API_URL)
       .then((res) => {
-        setMovie(res.data.results[0]);
+        setMovie(res.data.results[Math.floor(Math.random() * (res.data.results.length - 1))]);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  if (movie === undefined) return <div>Loading...</div>;
   return (
     <div
       className="banner"
@@ -26,17 +29,27 @@ function Banner() {
       }}
     >
       <div className="banner__text">
-        <h1 className="banner__text__title">Black Crab</h1>
+        <h1 className="banner__text__title">{movie.original_title}</h1>
         <div className="banner__text__desc">
-          asdjasdbjkasdhjaskdhjaskdhjasdh sdh asdh lasdhlkasdhjklasdjklas
+          {truncate(movie?.overview, 150)}
         </div>
-        <div className="banner__text__btn">
-          <button>Play</button>
-          <button>More Info</button>
+        <div className="banner__text__btns">
+          <button className="banner__text__btn play__btn">
+            <PlayArrowRoundedIcon className="btn__icon"/>
+            <span>Play</span>
+          </button>
+          <button className="banner__text__btn info__btn">
+            <InfoOutlinedIcon className="btn__icon"/>
+            <span>More Info</span>
+          </button>
         </div>
       </div>
     </div>
   );
+}
+
+function truncate(str, n) {
+  return str?.length > n ? str.substr(0, n - 1) + " ..." : str;
 }
 
 export default Banner;
