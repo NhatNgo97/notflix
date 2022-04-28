ae0357e06df3d23e74a6a51433dc36a89460a32f
NhatNgo97 <nhatngo9799@gmail.com>
2022-04-28 19:54:31 +0700

    footer

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,20 @@
 import { useNavigate } from "react-router";
 import Banner from "../../components/banner/banner";
 import MovieRow from "../../components/movieRow/movieRow";
+import Footer from "../../components/footer/footer";
+
 
 function Home() {
 
   return (
     <div>
       <Banner />
-      <MovieRow title="Top Rated" fetchUrl="popular" />
-      <MovieRow title="ABCD" fetchUrl="popular" />
+      <div>
+        <MovieRow title="Top Rated" fetchUrl="popular" />
+        <MovieRow title="ABCD" fetchUrl="popular" />
+      </div>
+      <Footer />
+
     </div>
   );
 }
